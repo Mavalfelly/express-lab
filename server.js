9bff39bc063523241ccfb8c13d4d82344f713250
MaValFelly <matthewfeliciano16@gmail.com>
2024-07-24 17:16:16 -0400

    s

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+
+app.listen(3000, () => {//3000-8000 active ports    
+    console.log('listening on port 3000')
+})
+
+
 //section 1
 app.get('/greetings/:name', (req, res) => {
     res.send(`<h1>Hello ${req.params.name} how are you today?`)
@@ -11,8 +17,4 @@ app.get('/roll/:num', (req, res) => {
     res.send(`<h1>You rolled a  ${Math.floor(Math.random()*(req.params.num-0))}`)
 })
 
-//
-
-app.listen(3000, () => {//3000-8000 active ports    
-    console.log('listening on port 3000')
-})
+//section 3
